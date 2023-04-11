6efb19768ae75b62e7a72f69a1e1aa87aad10c9b
Gangadhar C <gangadharganga90@gmail.com>
2023-04-11 12:41:28 +0530

    Add project files.

---
+++ Pages/Books/CreateBook.cshtml.cs
@@ -0,0 +1,44 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace AspDotNETWebApplication1.Pages.Books
+{
+    public class CreateBook : PageModel
+    {
+        public string Message="";
+        public void OnGet()
+        {
+        }
+        public void OnPost() 
+        {
+            try
+            { 
+                Books book = new Books();
+                book.Id = Request.Form["Id"];
+                book.Title = Request.Form["Title"];
+                book.Author = Request.Form["Author"];
+                book.Publication = Request.Form["Publication"];
+                book.Price = Convert.ToDouble(Request.Form["Price"].ToString());
+                using (SqlConnection sqlConnection = new SqlConnection(SQLServerConnect.conn))
+                {
+                    sqlConnection.Open();
+                    using (SqlCommand cmd = sqlConnection.CreateCommand())
+                    {
+                        cmd.CommandText = $"INSERT INTO LMS_BOOK_DETAILS (BOOK_CODE,BOOK_TITLE,AUTHOR, PUBLICATION, PRICE) VALUES('{book.Id}','{book.Title}','{book.Author}','{book.Publication}',{book.Price})";
+                        cmd.ExecuteNonQuery();
+                        Message = "Added Successfully";
+                    }
+                }
+            }
+            catch (SqlException sqlEx) 
+            {
+                Message = sqlEx.Message;
+            }
+            catch(Exception ex)
+            {
+                Message = ex.Message;
+            }
+        }
+    }
+}
---
+++ Pages/Books/DisplayBooks.cshtml.cs
@@ -0,0 +1,56 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.SqlClient;
+
+namespace AspDotNETWebApplication1.Pages.Books
+{
+    public class DisplayBooks : PageModel
+    {
+        public List<Books> BookList= new List<Books>();
+        public void OnGet()
+        {
+            
+            try 
+            {
+                using (SqlConnection sqlConnection = new SqlConnection(SQLServerConnect.conn))
+                {
+                    sqlConnection.Open();
+                    using (SqlCommand cmd = sqlConnection.CreateCommand())
+                    {
+                        cmd.CommandText = "SELECT BOOK_CODE, BOOK_TITLE, AUTHOR, PUBLICATION, PRICE " +
+                                            "FROM LMS_BOOK_DETAILS";
+                        SqlDataReader reader =cmd.ExecuteReader();
+                        while (reader.Read())
+                        {
+                            Books book = new Books();
+                            book.Id = reader[0].ToString();
+                            book.Title= reader[1].ToString();
+                            book.Author = reader[2].ToString();
+                            book.Publication = reader[3].ToString();
+                            book.Price = (double)reader[4];
+                            BookList.Add(book);
+                        }
+                    }
+
+                }
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch(Exception ex) 
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+    public class Books
+    {
+        public string Id { get; set; }
+        public string Title { get; set; }
+        public string Category { get; set; }
+        public string Author { get; set; }
+        public string Publication { get; set; }
+        public double Price { get; set; }
+    }
+}
